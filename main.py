e59344f71cdaa9e16e83296f84a2dfb5114688eb
Marcelo Rojas <03rojasmarcelo@gmail.com>
2025-05-21 15:38:18 -0400

    Cambios en avance de transiciones

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from collections import deque
 
-#arreglar el problema de la pila con las transiciones duplicadas
 if __name__ == "__main__":
     transiciones = [(0, 'a', 'R', 0, 'AR'), (0, 'a', 'A', 0, 'AA'), (0, 'b', 'A', 1, '-'),
                      (1, 'b', 'A', 1, '-'), (1, '-', 'R', 2, 'R')]
@@ -9,27 +8,29 @@ if __name__ == "__main__":
     pila = deque(['R'])
     palabra = input("Ingrese la palabra a procesar: ")
 
-    
-
+#Problema: con este for no se alcanza a llegar a estado final
     for i in range(len(palabra)):
         simbolo_actual = palabra[i]
-        resultado = next((t for t in transiciones if t[0] == estado_actual and t[1] == simbolo_actual), None)
+        print("Simbolo actual: ", simbolo_actual)
+        print("Estado actual: ", estado_actual)
+        print("Tope de pila: ", pila[0])
+        
+        #Cambio: ahora tambien busca la transición que coincida con tope de pila
+        resultado = next((t for t in transiciones if (t[0] == estado_actual and t[1] == simbolo_actual) and t[2] == pila[0]), None)
+        print("Transicion encontrada: ", resultado, "\n")
         if not resultado:
             print(f"Error: No hay transición definida para el estado {estado_actual} con el símbolo '{simbolo_actual}'")
-            break
-        if not resultado[2] == pila[0]:
-            print(f"Error: No se puede hacer la transición desde el estado {estado_actual} con el símbolo '{simbolo_actual}'")
             break
          
         estado_actual = resultado[3]
         if len(resultado[4]) > 1:
-            for j in range(len(resultado[4])-2, 0, -1):
+            for j in range(len(resultado[4])-2, -1, -1):
                 pila.appendleft(resultado[4][j])
         elif resultado[4] == '-':
             pila.popleft()
         
-    if estado_actual == estado_final:
-        print("Cadena aceptada por estado final")
+    '''if estado_actual == estado_final:
+        print("Cadena aceptada por estado final")'''
         
 
 
